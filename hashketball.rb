be3e6f3b72d57da3e7ae00defee470285c74a445
Henry Velasquez <hvelas93@gmail.com>
2021-02-20 02:24:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,6 @@
 # Write your code below game_hash
+
+require 'pry'
 def game_hash
   {
     home: {
@@ -126,4 +128,85 @@ def game_hash
   }
 end
 
+# final_results=data.each_with_object({}) do |(key,value), final_array|
+#         #binding.pry
+#         value.each do |inner_key,names|
+#           names.each do |name|
+#             #binding.pry
+#             if !final_array[name]
+#               final_array[name]={}
+#               #final_array<<name
+#             end
+#             if !final_array[name][key]
+#               !final_array[name][key]=[]
+#             end
+#         final_array[name][key].push(inner_key.to_s)
+#           end
+#        end
+#      end
+
 # Write code here
+
+def num_points_scored(player_name)
+
+scored_points=""
+
+    game_hash[:home][:players].each do |index|
+      if index[:player_name]==player_name
+        scored_points=index[:points]
+      end
+
+    end
+
+    game_hash[:away][:players].each do |index2|
+      if index2[:player_name]==player_name
+        scored_points=index2[:points]
+      end
+
+    end
+scored_points
+#binding.pry
+end
+
+def shoe_size(player_name2)
+
+  shoe_sizes=""
+
+      game_hash[:home][:players].each do |index|
+        if index[:player_name]==player_name2
+          shoe_sizes=index[:shoe]
+        end
+
+      end
+
+
+      game_hash[:away][:players].each do |index2|
+        if index2[:player_name]==player_name2
+          shoe_sizes=index2[:shoe]
+        end
+        #binding.pry
+end
+shoe_sizes
+end
+
+def team_colors(team_name)
+
+team_coloring=""
+
+if game_hash[:home][:team_name]==team_name
+  team_coloring=game_hash[:home][:colors]
+end
+
+  if game_hash[:away][:team_name]==team_name
+    team_coloring=game_hash[:away][:colors]
+  end
+
+team_coloring
+end
+
+def team_names
+teams_playing=Array.new
+  teams_playing<< game_hash[:home][:team_name]
+  teams_playing<< game_hash[:away][:team_name]
+
+end
